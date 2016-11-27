70efc177403a7d262fdb100d2a0d213aba9cdc8f
Ian Mallett <incompetent.qed@gmail.com>
2016-11-27 15:58:48 +1100

    Beginning UI Work
    Added dragging camera controls, and framework for selection.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,17 +3,45 @@ using System.Collections;
 
 public class CameraController : MonoBehaviour {
 
+	public float initialDownAngle;
+	public float initialYAxis;
+	public float initialOffset;
+	public Vector3 startingLookPos;
+
+	public GameController gameController;
+
+	//Layer 8 is clickable
+	private int clickableMask = 1 << 8;
+	//Layer 9 is ground
+	private int groundMask = 1 << 9;
+	//Layer 10 is untargetable
+	private int untargetableMask = 1 << 10;
+
+	//Camera movement management
+	private Vector3 dragPoint;
+	private bool holding = false;
+	private bool dragging = false;
+
+	//Camera position storage
 	public float downAngle;
 	public float yAxis;
 	public float offset;
+	private Vector3 lookPosition;
 
-	public Vector2 startingPos;
+	//This threshold is in world units
+	public float dragThreshold;
 
 	public float moveSpeed;
+
 
 	// Use this for initialization
 	void Start () {
 		setTransformFromParameters ();
+		downAngle = initialDownAngle;
+		yAxis = initialYAxis;
+		offset = initialOffset;
+		lookPosition = startingLookPos;
+		gameController = GameObject.FindObjectOfType<GameController> ();
 	}
 	
 	// Update is called once per frame
@@ -32,22 +60,67 @@ public class CameraController : MonoBehaviour {
 			if (Input.GetKey (KeyCode.DownArrow)) {
 				motion.z = motion.z - moveSpeed;
 			}
-			gameObject.transform.Translate (motion, Space.World);
+			lookPosition = lookPosition + motion;
 		}
+
+		if (Input.GetMouseButton (0)) {
+			if (holding) {
+				RaycastHit hit;
+				Ray click = Camera.main.ScreenPointToRay (Input.mousePosition);
+				if (Physics.Raycast (click, out hit, 100, groundMask | clickableMask)) {
+					if (dragging || Vector3.Distance(hit.point, dragPoint) >= dragThreshold) {
+						dragging = true;
+						float distanceToPointHeight = (dragPoint.y - click.origin.y)/click.direction.y;
+						lookPosition = lookPosition + (dragPoint - click.GetPoint (distanceToPointHeight));
+					}
+				}
+			}
+		}
+
+		if (Input.GetMouseButtonDown (0)) {
+			RaycastHit hit;
+			Ray click = Camera.main.ScreenPointToRay (Input.mousePosition);
+			if (Physics.Raycast (click, out hit, 100, groundMask | clickableMask)) {
+				holding = true;
+				dragPoint = hit.point;
+			}
+		}
+
+		if (Input.GetMouseButtonUp (0)) {
+			if (!dragging) {
+				clickOn (Input.mousePosition);
+			}
+			holding = false;
+			dragging = false;
+		}
+
+		positionCamera ();
+	}
+
+	private void clickOn(Vector3 mousePosition) {
+
+	}
+
+	private void positionCamera() {
+		gameObject.transform.rotation = Quaternion.Euler (downAngle, 0, 0);
+		gameObject.transform.position = new Vector3 (lookPosition.x,
+													 yAxis,
+													 lookPosition.z + offset);
 	}
 
 	[ContextMenu("Set Parameters")]
 	void setParametersFromTransform () {
-		downAngle = gameObject.transform.rotation.eulerAngles.x;
-		yAxis = gameObject.transform.position.y;
-		offset = gameObject.transform.position.z;
+		initialDownAngle = gameObject.transform.rotation.eulerAngles.x;
+		initialYAxis = gameObject.transform.position.y;
+		initialOffset = gameObject.transform.position.z;
 	}
 
-	[ContextMenu("Set Positions")]
+	[ContextMenu("Set Position")]
 	void setTransformFromParameters () {
-		gameObject.transform.rotation = Quaternion.Euler (downAngle, 0, 0);
-		gameObject.transform.position = new Vector3 (startingPos.x,
-			yAxis,
-			startingPos.y + offset);
+		gameObject.transform.rotation = Quaternion.Euler (initialDownAngle, 0, 0);
+		gameObject.transform.position = new Vector3 (startingLookPos.x,
+			initialYAxis,
+			startingLookPos.z + initialOffset);
 	}
+
 }
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public enum State {
+		Waiting,
+		Selected,
+		SelectingGround,
+		SelectingClickable,
+		Animating
+	};
+
+	public State currentState = State.Waiting;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void selectUnit(GameObject unitObject) {
+		print ("Selected unit " + unitObject.GetInstanceID ());
+	}
+
+	public bool isOwnUnit(GameObject unitObject) {
+		return true;
+	}
+}
